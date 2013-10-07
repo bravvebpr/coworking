6d0f47ed6c8ff4f7781c8496356c6cdb0cf375e0
Oscar Bravo <osquitarbs@gmail.com>
2013-10-07 12:02:02 +0200

    solucionando problema de scroll

--- cartel_4.php
+++ cartel_4.php
@@ -1,6 +1,6 @@
 <div class="cartel_4" id="cartel_4">
     <div class="contenedor_4">
-       <!--<div id="acdc_4" style="position: absolute;height: 1px;width: 10%;margin-top: 200px"></div>-->
+<!--       <div id="acdc_4" style="position:absolute;height: 1px;width: 100%;margin-top: 200px"></div>-->
         <div>
             <?php for($x=0; $x<4; $x++)
                 echo '<div class="lavadoras_cont"><img src="img/lavadora.png" alt="" class="lavadoras"><img src="img/bombo.png" alt="" class="bombo"></div>';
@@ -12,7 +12,7 @@
                 echo '<div class="lavadoras_cont"><img src="img/lavadora.png" alt="" class="lavadoras"><img src="img/bombo.png" alt="" class="bombo"></div>'; 
             ?>
         </div>
-        <div class="contenido_lavadora">
+<div class="contenido_lavadora" id="acdc_4">
             <span class="letras_lavadoras" id="poner">&Oacute; PONER</span> <span class="numero_lavadoras" id="num_lavadoras">11</span><br/>
             <span class="letras_lavadoras" id="let_lavadoras">LAVADORAS</span> 
         </div>
