b5ba96e6c8cf088bc09bd18dabf51b39092016b4
alberto <albertominaya91@gmail.com>
2013-10-09 12:00:20 +0200

    realizando email de contacto

--- contacto.php
+++ contacto.php
@@ -10,31 +10,31 @@
         <script src="lib/validar.js"></script>
         <script src="lib/jquery.scrollExtend.min.js"></script>
     </head>
-    <body>
-        <div class="contacto" id="contacto">
-            <div class="contenedor_contacto">
-                <?php include './cabecera.php'; ?>
-                <form action="lib/envio_mensaje.php" method="post">
-                    <input type="hidden" name="img_select" id="img_select" value="">
-                    <div class="formulary">
-                        <div class="apartados_form3">
-                            <span>Email:</span> <input type="text" style="width: 345px;" name="email" id="email"><br/><br/>
-                            <span>Asunto:</span> <input type="text" style="width: 334px;" name="asunto" id="asunto"><br/><br/>
-                        </div>
-                        <div class="apartados_form4">
-                            <span>Nombre:</span> <input type="text" style="width: 252px;" name="nombre" id="nombre"><br/><br/>
-                            <span>Telefono:</span> <input type="text" style="width: 244px;" name="number" id="number"><br/><br/>
-                        </div>
+    <body style="background: #f34d25;">
+        <div class="contenedor_contacto">
+            <?php include './cabecera.php'; ?>
+            <div class="cont_form">
+            <form action="lib/envio_mensaje.php" method="post">
+                <input type="hidden" name="img_select" id="img_select" value="">
+                <div class="formulary">
+                    <div class="apartados_form3">
+                        <span>Email:</span> <input type="text" style="width: 345px;" name="email" id="email"><br/><br/>
+                        <span>Asunto:</span> <input type="text" style="width: 334px;" name="asunto" id="asunto"><br/><br/>
                     </div>
-                    <div class="limpiar"></div>
-                    <div class="mensaje">
-                        <textarea type="text" name="mensaje" id="mensaje"></textarea>
+                    <div class="apartados_form4">
+                        <span>Nombre:</span> <input type="text" style="width: 252px;" name="nombre" id="nombre"><br/><br/>
+                        <span>Telefono:</span> <input type="text" style="width: 244px;" name="number" id="number"><br/><br/>
                     </div>
-                    <div class="boton_ayudar">
-                        <input type="button" value="Enviar" onclick="validarCorreo(this.form);">
-                    </div>
-                </form>
-            </div>
+                </div>
+                <div class="limpiar"></div>
+                <div class="mensaje">
+                    <textarea type="text" name="mensaje" id="mensaje"></textarea>
+                </div>
+                <div class="boton_ayudar">
+                    <input type="button" value="Enviar" onclick="validarCorreo(this.form);">
+                </div>
+            </form>
+           </div>
         </div>
     </body>
 </html>
--- lib/envio_mensaje.php
+++ lib/envio_mensaje.php
@@ -2,17 +2,20 @@
 
 $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-$headers .= "From: Co-working.me <hoyenelmundoweb1@gmail.com>\r\n";
+$headers .= 'From: Co-working.me <' . $_POST['email'] . '>\r\n';
 $mail = $_POST['email'];
 $asunto = $_POST['asunto'];
+
+if($_POST['number']=="")
+    $sw_nn = "no ha dejado numero";
+else
+    $sw_nn = "su numero es " .$_POST['number'] ."";
 $mensaje = '
-<p style="background-color:#f34d25;background-image:-moz-linear-gradient(center top,#d10a0d,#650506);background:-moz-linear-gradient(top,#d10a0d,#650506);background:-o-linear-gradient(top,#d10a0d,#650506);border:1px solid #6e6e6e;color:white;font-size:1.2em;font-weight:bold;text-align:center;padding-bottom:1%;padding-top:1%;border-radius:1em">
-   ' . $_POST['nombre'] . ' quiere contactar contigo, su numero de telefono es '. $_POST['number'] . '</p>
-  </p><p style="background-color:#b7f5ec;background-image:-moz-linear-gradient(center top,#d10a0d,#650506);background:-moz-linear-gradient(top,#d10a0d,#650506);background:-o-linear-gradient(top,#d10a0d,#650506);border:1px solid #6e6e6e;color:#000000;font-size:14px;font-weight:bold;text-align:center;padding-bottom:1%;padding-top:1%;border-radius:1em"><strong>Mensaje: </strong>' . $_POST['mensaje'] . ' 
-      <p>
-';
-if (mail($mail, $asunto, $mensaje, $headers)){
-   
+<p> ' . $_POST['nombre'] . ' quiere contactar contigo,' . $sw_nn . '</p>
+  <p><strong>Mensaje: </strong>' . $_POST['mensaje'] . ' 
+   ';
+if (mail($mail, $asunto, $mensaje, $headers)) {
+
     echo $asunto;
     echo $mensaje;
 }
