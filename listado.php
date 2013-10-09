23af11d74c1046e3f66c39dd819da1debe09e8e3
Oscar Bravo <osquitarbs@gmail.com>
2013-10-09 11:53:53 +0200

    creando panel de gestion

--- listado.php
+++ listado.php
@@ -1,21 +1,21 @@
 <?php
-//	session_start();
-//	if ($_SESSION["usuario"]=="")
-//		header("Location: login.php");
-        
+//session_start();
+//if ($_SESSION["usuario"] == "")
+//    header("Location: login.php");
 ?>
 <?php
-    $conexion = mysql_connect ("db496705092.db.1and1.com","dbo496705092","Anab5210");
-    mysql_select_db("db496705092",$conexion);
-    $datos=mysql_query("SELECT * FROM centros");
+$conexion = mysql_connect("localhost", "proyecto", "proyecto");
+mysql_select_db("proyecto", $conexion);
+$datos = mysql_query("SELECT * FROM centros");
 ?>
+<!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-        <meta name="�viewport�" content="�width=device-width," initial-scale="1?">
-        <title>eShow</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>co-working.me</title>
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="css/reset.css">
-        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <link rel="stylesheet" type="text/css" href="css/estilos_panel.css">
         <link rel="stylesheet" type="text/css" href="css/demo_page.css">
         <link rel="stylesheet" type="text/css" href="css/demo_table_jui.css">
         <link rel="stylesheet" type="text/css" href="css/jquery-ui-1.8.4.custom.css">
@@ -23,50 +23,63 @@
         <script src="lib/jquery.dataTables.js"></script>
         <script>
             $(document).ready(function() {
-            oTable = $('#example').dataTable({
-                "bJQueryUI": true,
-                "sPaginationType": "full_numbers"
+                oTable = $('#example').dataTable({
+                    "bJQueryUI": true,
+                    "sPaginationType": "full_numbers"
+                });
             });
-        } );
         </script>
     </head>
-<body>
-    <header style="width: 1336px; margin: 0 auto;">
-        <div class="logo" style="margin-bottom: 20px;"><img src="img/logo.png"></div>
-        <?php
-               // echo 'Usuario: '.$_SESSION["usuario"];
-        ?>
-        <a href="lib/cerrar_sesion" class="cerrar_sesion">Cerrar sesion</a>
-    </header>
-<table cellpadding="0" cellspacing="0" border="0" class="display dataTable" id="example" aria-describedby="example_info">
-	<thead>
-		<tr role="row">
+    <body>
+        <table cellpadding="0" cellspacing="0" border="0" class="display dataTable" id="example" aria-describedby="example_info">
+            <thead>
+                <tr role="row">
                     <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1"  aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">
-                        <div class="DataTables_sort_wrapper">Localidad</div>
-                    </th>
-                    <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">
-                        <div class="DataTables_sort_wrapper">Direcci&oacute;n</div>
-                    </th>
-                    <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">
-                        <div class="DataTables_sort_wrapper">Imagen</div>
-                    </th>
-                    <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" >
-                        <div class="DataTables_sort_wrapper">Acciones</div>
-                    </th>
-                </tr>
-	</thead>
-        <tbody role="alert" aria-live="polite" aria-relevant="all">
-        <?php
-          	  while ($fila = mysql_fetch_array($datos)){
-          ?>
-            <tr>
-                <td><?php echo $fila['localidad']; ?></td>
-                <td><?php echo $fila['direccion']; ?></td>
-                <td><?php echo $fila['imagen']; ?></td>
-                <td>editar borrar</td>
-            </tr>
-        <?php } ?>
-        </tbody>
+            <div class="DataTables_sort_wrapper">ID</div>
+        </th>
+        <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">
+        <div class="DataTables_sort_wrapper">Localidad</div>
+    </th>
+    <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">
+    <div class="DataTables_sort_wrapper">Nombre</div>
+</th>
+<th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" >
+<div class="DataTables_sort_wrapper">Telefono</div>
+</th>
+<th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">
+<div class="DataTables_sort_wrapper">Direccion</div>
+</th>
+<th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">
+<div class="DataTables_sort_wrapper">e-mail</div>
+</th>
+<th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">
+<div class="DataTables_sort_wrapper">Imagen</div>
+</th>
+<th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 300px!important;">
+<div class="DataTables_sort_wrapper">URL</div>
+</th>
+<th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 300px!important;">
+<div class="DataTables_sort_wrapper">Acciones</div>
+</th>
+</tr>
+</thead>
+<tbody role="alert" aria-live="polite" aria-relevant="all">
+    <?php
+    while ($fila = mysql_fetch_array($datos)) {
+        ?>
+        <tr>
+            <td><?php echo $fila['id']; ?></td>
+            <td><?php echo $fila['localidad']; ?></td>
+            <td><?php echo $fila['nombre']; ?></td>
+            <td><?php echo $fila['tlf']; ?></td>
+            <td><?php echo $fila['direccion']; ?></td>
+            <td><?php echo $fila['descripcion']; ?></td>
+            <td><?php echo $fila['imagen']; ?></td>
+            <td><?php echo $fila['url']; ?></td>
+            <td>Editar Borrar</td>
+        </tr>
+    <?php } ?>
+</tbody>
 </table>
 </body>
 </html>
