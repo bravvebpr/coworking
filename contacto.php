f3147619a62a00cd83d4900d322b810d4d859cd6
alberto <albertominaya91@gmail.com>
2013-10-09 13:34:30 +0200

    acabado formulario de contactos y diseño demostrar correo mejorado

--- contacto.php
+++ contacto.php
@@ -14,27 +14,22 @@
         <div class="contenedor_contacto">
             <?php include './cabecera.php'; ?>
             <div class="cont_form">
-            <form action="lib/envio_mensaje.php" method="post">
-                <input type="hidden" name="img_select" id="img_select" value="">
-                <div class="formulary">
-                    <div class="apartados_form3">
-                        <span>Email:</span> <input type="text" style="width: 345px;" name="email" id="email"><br/><br/>
-                        <span>Asunto:</span> <input type="text" style="width: 334px;" name="asunto" id="asunto"><br/><br/>
-                    </div>
-                    <div class="apartados_form4">
-                        <span>Nombre:</span> <input type="text" style="width: 252px;" name="nombre" id="nombre"><br/><br/>
-                        <span>Telefono:</span> <input type="text" style="width: 244px;" name="number" id="number"><br/><br/>
-                    </div>
-                </div>
-                <div class="limpiar"></div>
-                <div class="mensaje">
-                    <textarea type="text" name="mensaje" id="mensaje"></textarea>
-                </div>
-                <div class="boton_ayudar">
-                    <input type="button" value="Enviar" onclick="validarCorreo(this.form);">
-                </div>
-            </form>
-           </div>
+                <form action="lib/envio_mensaje.php" method="post">
+                        <div class="apartados_form3">
+                            <span>Email:</span><br/> <input type="text" style="width: 345px;" name="email" id="email" placeholder="Email"><br/><br/>
+                            <span>Asunto:</span><br/> <input type="text" style="width: 345px;" name="asunto" id="asunto" placeholder="Asunto"><br/><br/>
+                            <span>Nombre:</span><br/> <input type="text" style="width: 345px;" name="nombre" id="nombre" placeholder="Nombre"><br/><br/>
+                            <span>Telefono:</span><br/> <input type="text" style="width: 345px;" name="number" id="number" placeholder="Telefono">
+                        </div>
+                        <div class="limpiar"></div>
+                        <div class="mensaje">
+                            <textarea type="text" name="mensaje" id="mensaje" placeholder="Mensaje"></textarea>
+                        </div>
+                        <div class="boton_ayudar">
+                            <input type="button" value="Enviar" onclick="validarCorreo(this.form);">
+                        </div>
+                </form>
+            </div>
         </div>
     </body>
 </html>
--- lib/envio_mensaje.php
+++ lib/envio_mensaje.php
@@ -4,15 +4,15 @@ $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 $headers .= 'From: Co-working.me <' . $_POST['email'] . '>\r\n';
 $mail = $_POST['email'];
-$asunto = $_POST['asunto'];
+$asunto ='<p><strong style="font-size:23px;">' . $_POST['asunto'] . '</strong>';
 
 if($_POST['number']=="")
-    $sw_nn = "no ha dejado numero";
+    $sw_nn = "No ha dejado numero";
 else
     $sw_nn = "su numero es " .$_POST['number'] ."";
-$mensaje = '
-<p> ' . $_POST['nombre'] . ' quiere contactar contigo,' . $sw_nn . '</p>
-  <p><strong>Mensaje: </strong>' . $_POST['mensaje'] . ' 
+$mensaje = '<p><strong style="font-size:18px;">' . $_POST['nombre'] . '</strong> quiere contactar contigo.</br></br>
+    <strong style="font-size:18px;">Telefono:</strong></br>' . $sw_nn . '</p>
+  <p><strong style="font-size:18px;">Mensaje:</strong></br> ' . $_POST['mensaje'] . ' 
    ';
 if (mail($mail, $asunto, $mensaje, $headers)) {
 
