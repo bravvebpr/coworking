63cdf0705f87af673f4bd2b6e48ef16c6cab4b39
alberto <albertominaya91@gmail.com>
2013-10-11 10:24:34 +0200

    modificando enlaces

--- cabecera.php
+++ cabecera.php
@@ -3,8 +3,8 @@
     <i>Lo est&aacute; pidiedo a gritos</i>
 </div>
 <div class="index_cabecera">
-    <a href="#">Filosiof&iacute;a</a>
-    <a href="#">Quienes somos</a>
+    <a href="./filosofia.php">Filosiof&iacute;a</a>
+    <a href="./historial.php">Quienes somos</a>
     <a href="./contacto.php">Contacto</a>
 </div>
 <div class="limpiar"></div>
---
+++ historial.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Quienes somos</title>
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script src="lib/validar.js"></script>
+        <script src="lib/jquery.scrollExtend.min.js"></script>
+    </head>
+    <body style="background: #f34d25; padding-top: 30px;">
+        <div class="contenedor_historial">
+            <?php include './cabecera.php'; ?>
+            
+        </div>
+    </body>
+</html>
