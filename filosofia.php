468d2713ed558a3af3d67a6a224e398f1ddb2c85
alberto <albertominaya91@gmail.com>
2013-10-11 15:51:54 +0200

    prueba

--- filosofia.php
+++ filosofia.php
@@ -1,3 +1,72 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Quienes somos</title>
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script src="lib/validar.js"></script>
+        <script src="lib/jquery.scrollExtend.min.js"></script>
+    </head>
+    <body style="background: #f34d25; padding-top: 30px;">
+        <div class="contenedor_historial">
+            <?php include './cabecera.php'; ?>
+            <div class="contenedor_log">
+                <div class="titulo_cont"> 
+                    Pellentesque nec magna ac enim.
+                </div>
+                <div class="texto_1">
+                    <strong style="color: #62d5f7;">1. Cu&eacute;ntale que es el coworking de una manera divertida.<br/></strong>
+                    <span> 
+                        Mauris a laoreet quam. Nunc ut eros in augue vulputate convallis. Praesent a tellus eget sem commodo porttitor sit amet id magna.
+                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                        Quisque hendrerit urna a dolor tristique, non tincidunt felis sollicitudin.
+                        Donec vitae imperdiet diam. Vivamus blandit non odio at porttitor. Vivamus pharetra faucibus eros sit amet imperdiet. 
+                        Integer eleifend dictum elit, egestas pellentesque turpis. Sed id eleifend elit.
+                    </span>
+                </div>
 
-?>
+                <div class="texto_1">
+                    <strong style="color: #62d5f7;">
+                        2. Curabitur consectetur pharetra ipsum, et commodo ligula mattis non.<br/>
+                    </strong>
+                    <span>
+                        Duis ut erat interdum, aliquet nibh eu, pulvinar lorem. Praesent tempus sed urna sed porttitor. In hac habitasse platea dictumst.
+                        Sed ultricies turpis vel odio iaculis, interdum posuere odio vulputate. Nullam accumsan nulla nisi, a placerat erat malesuada quis.
+                        Morbi tempor ipsum nibh, ac rutrum lorem hendrerit id. Praesent ullamcorper libero ut viverra fringilla.
+                        Phasellus ut purus suscipit, scelerisque sapien a, volutpat nibh.
+                    </span>
+                </div>
+          
+            <div class="titulo_cont"> 
+                Pellentesque nec magna ac enim.
+            </div>
+            <div class="texto_1">
+                <strong style="color: #62d5f7;">1. Cu&eacute;ntale que es el coworking de una manera divertida.<br/></strong>
+                <span> 
+                    Mauris a laoreet quam. Nunc ut eros in augue vulputate convallis. Praesent a tellus eget sem commodo porttitor sit amet id magna.
+                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    Quisque hendrerit urna a dolor tristique, non tincidunt felis sollicitudin.
+                    Donec vitae imperdiet diam. Vivamus blandit non odio at porttitor. Vivamus pharetra faucibus eros sit amet imperdiet. 
+                    Integer eleifend dictum elit, egestas pellentesque turpis. Sed id eleifend elit.
+                </span>
+            </div>
+
+            <div class="texto_1">
+                <strong style="color: #62d5f7;">
+                    2. Curabitur consectetur pharetra ipsum, et commodo ligula mattis non.<br/>
+                </strong>
+                <span>
+                    Duis ut erat interdum, aliquet nibh eu, pulvinar lorem. Praesent tempus sed urna sed porttitor. In hac habitasse platea dictumst.
+                    Sed ultricies turpis vel odio iaculis, interdum posuere odio vulputate. Nullam accumsan nulla nisi, a placerat erat malesuada quis.
+                    Morbi tempor ipsum nibh, ac rutrum lorem hendrerit id. Praesent ullamcorper libero ut viverra fringilla.
+                    Phasellus ut purus suscipit, scelerisque sapien a, volutpat nibh.
+                </span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+  
--- pie.php
+++ pie.php
@@ -5,7 +5,7 @@
             <div class="cerrar" id="cerrar">
                 <img src="img/mas_pop-up.png" alt="">
             </div>
-                <img alt="" id="img_pop_gr" src="">
+            <img alt="" id="img_pop_gr" src="">
         </div>
         <!---------------------------------------------------------------------------->     
 
@@ -77,7 +77,7 @@
                                 <option>Leganes</option>
                             </select>
                         </div>
-                            <br/><br/>
+                        <br/><br/>
                         <span>El email de tu amigo:</span> <input type="text" style="width: 252px;;" name="correo" id="correo"><br/>
                     </div>
                 </div>
@@ -88,23 +88,38 @@
             </form>
             <div class="limpiar"></div>
             <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://alfadentalteam.es" data-lang="es">Twittear</a>
-                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-                
-             <div data-href="https://alfadentalteam.es" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-show-faces="true" data-send="false" class="fb-like fb_edge_widget_with_comment fb_iframe_widget" fb-xfbml-state="rendered"><span style="height: 29px; width: 450px;"><iframe id="fe22dbfd" name="f35ff6258c" scrolling="no" title="Like this content on Facebook." class="fb_ltr" src="https://www.facebook.com/plugins/like.php?api_key=113869198637480&amp;channel_url=https%3A%2F%2Fs-static.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D27%23cb%3Df380100104%26domain%3Ddevelopers.facebook.com%26origin%3Dhttps%253A%252F%252Fdevelopers.facebook.com%252Ff1a437ec78%26relation%3Dparent.parent&amp;colorscheme=light&amp;extended_social_context=false&amp;href=https%3A%2F%2Falfadentalteam.es&amp;layout=standard&amp;locale=es_ES&amp;node_type=link&amp;sdk=joey&amp;send=false&amp;show_faces=true&amp;width=450" style="border: none; overflow: hidden; height: 29px; width: 450px;"></iframe></span></div>
-            
+            <script>!function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                            if (!d.getElementById(id)) {
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = p + '://platform.twitter.com/widgets.js';
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }
+                        }(document, 'script', 'twitter-wjs');</script>
+
+            <div data-href="https://alfadentalteam.es" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-show-faces="true" data-send="false" class="fb-like fb_edge_widget_with_comment fb_iframe_widget" fb-xfbml-state="rendered">
+                <span style="height: 29px; width: 450px;">
+                    <iframe id="fe22dbfd" name="f35ff6258c" scrolling="no" title="Like this content on Facebook." class="fb_ltr" src="https://www.facebook.com/plugins/like.php?api_key=113869198637480&amp;channel_url=https%3A%2F%2Fs-static.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D27%23cb%3Df380100104%26domain%3Ddevelopers.facebook.com%26origin%3Dhttps%253A%252F%252Fdevelopers.facebook.com%252Ff1a437ec78%26relation%3Dparent.parent&amp;colorscheme=light&amp;extended_social_context=false&amp;href=https%3A%2F%2Falfadentalteam.es&amp;layout=standard&amp;locale=es_ES&amp;node_type=link&amp;sdk=joey&amp;send=false&amp;show_faces=true&amp;width=450" style="border: none; overflow: hidden; height: 29px; width: 450px;"></iframe>
+                </span>
+            </div>
+
             <!-- Place this tag where you want the share button to render. -->
             <div class="g-plus" data-action="share" data-href="http://alfadentalteam.es/"></div>
 
             <!-- Place this tag after the last share tag. -->
             <script type="text/javascript">
-              window.___gcfg = {lang: 'es'};
+                window.___gcfg = {lang: 'es'};
 
-              (function() {
-                var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-                po.src = 'https://apis.google.com/js/plusone.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-              })();
+                (function() {
+                    var po = document.createElement('script');
+                    po.type = 'text/javascript';
+                    po.async = true;
+                    po.src = 'https://apis.google.com/js/plusone.js';
+                    var s = document.getElementsByTagName('script')[0];
+                    s.parentNode.insertBefore(po, s);
+                })();
             </script>
-                
+
         </div>
     </div>
