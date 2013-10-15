a1b2ef71ed700786d593d9c1447dec8c7f29fb53
Oscar Bravo <osquitarbs@gmail.com>
2013-10-15 16:51:11 +0200

    arreglada lavadora y comienzo de animacion, subidos php de envio de correos

---
+++ correos/correo4.php
@@ -0,0 +1,82 @@
+<?php
+
+$cartel2='<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body style="background: #968479">
+        <table style="width: 800px; margin:0 auto; padding-top: 90px;" >
+            <tr>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+           </tr>
+           <tr>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td colspan="3">
+                    <img src="http://alfadentalteam.es/correos/img/pizza27.png" alt="">
+                </td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                
+           </tr>
+           <tr>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza_2.png" alt=""></td>
+                <td><img src="http://alfadentalteam.es/correos/img/pizza.png" alt=""></td>
+           </tr> 
+           <tr>
+               <td colspan="9" style="padding-top: 135px;">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae ligula tincidunt, sodales elit non, tempus purus. Quisque eget purus congue, luctus justo sit amet, sagittis metus. Fusce cursus, tortor in lacinia vehicula, arcu erat viverra metus, id ultricies ante sapien at dolor. Nullam et libero varius, mattis quam nec, rutrum orci. In ac ipsum dapibus est tempor pharetra vitae sit amet magna. Phasellus quis mauris sollicitudin, posuere enim accumsan, varius arcu. Mauris gravida est vel ligula suscipit fringilla. Aliquam erat volutpat. Nunc viverra tincidunt orci ut tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse ut justo eget urna ultricies tempor. Nam sit amet faucibus mauris. Aenean interdum mauris purus, tincidunt porta nisi euismod sed.
+                    Sed sollicitudin vestibulum est eu tincidunt. Aliquam in ante rutrum, pulvinar lorem at, ultrices sem. Morbi tempor volutpat convallis. Aliquam erat volutpat. Praesent dapibus dui nec commodo volutpat. Integer ullamcorper a dolor ac scelerisque. Curabitur vehicula nisi fermentum nulla eleifend, sed ullamcorper lacus porttitor. Sed neque nulla, accumsan at feugiat ut, feugiat a massa. Duis ac dignissim felis. Nam vel faucibus metus.
+                    Vivamus blandit egestas adipiscing. Morbi aliquet eu enim eget condimentum. Nam et bibendum tellus. Curabitur faucibus lectus at enim feugiat placerat. Mauris mattis ligula velit, et volutpat velit facilisis et. In vel massa convallis, commodo nibh non, porttitor orci. Vestibulum vestibulum felis at leo sodales ultricies. Mauris rhoncus vehicula ornare. Donec est ipsum, porta ac neque quis, ornare imperdiet mi. Nulla sit amet vehicula eros. Donec iaculis ante et risus blandit, ut euismod ligula tincidunt. Donec condimentum libero sit amet pharetra condimentum. Praesent eget tortor gravida, rutrum turpis vel, consectetur metus. Sed laoreet erat ante. Mauris eget elit urna.
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam ultricies molestie magna eu condimentum. Suspendisse sagittis dignissim felis a facilisis. In a ipsum arcu. Integer non auctor est, sed consequat turpis. Aliquam erat volutpat. Proin non laoreet sapien. Donec vitae posuere quam. Nullam sollicitudin turpis odio, quis elementum leo pretium vel. Etiam accumsan sodales sapien eu rhoncus. Maecenas sit amet sapien nec diam hendrerit congue eu at enim. Maecenas tempor egestas arcu, a ultricies eros dignissim sed. Praesent tristique orci libero, quis dignissim ante interdum id. Nam eu malesuada ipsum. Suspendisse dictum ipsum ac arcu venenatis fringilla. In id quam ultricies nulla porttitor dictum.
+                    Vivamus rhoncus diam et sagittis vulputate. Maecenas nisl lectus, pharetra ut venenatis sit amet, fringilla sodales elit. Nunc eget vulputate turpis, a mollis est. Nullam rhoncus ac ipsum sed pharetra. Fusce in risus laoreet nulla mattis mollis at a nunc. Maecenas egestas ultrices tellus nec ornare. Duis tempor tellus venenatis urna vulputate, at venenatis odio mollis.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae ligula tincidunt, sodales elit non, tempus purus. Quisque eget purus congue, luctus justo sit amet, sagittis metus. Fusce cursus, tortor in lacinia vehicula, arcu erat viverra metus, id ultricies ante sapien at dolor. Nullam et libero varius, mattis quam nec, rutrum orci. In ac ipsum dapibus est tempor pharetra vitae sit amet magna. Phasellus quis mauris sollicitudin, posuere enim accumsan, varius arcu. Mauris gravida est vel ligula suscipit fringilla. Aliquam erat volutpat. Nunc viverra tincidunt orci ut tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse ut justo eget urna ultricies tempor. Nam sit amet faucibus mauris. Aenean interdum mauris purus, tincidunt porta nisi euismod sed.
+                    Sed sollicitudin vestibulum est eu tincidunt. Aliquam in ante rutrum, pulvinar lorem at, ultrices sem. Morbi tempor volutpat convallis. Aliquam erat volutpat. Praesent dapibus dui nec commodo volutpat. Integer ullamcorper a dolor ac scelerisque. Curabitur vehicula nisi fermentum nulla eleifend, sed ullamcorper lacus porttitor. Sed neque nulla, accumsan at feugiat ut, feugiat a massa. Duis ac dignissim felis. Nam vel faucibus metus.
+                    Vivamus blandit egestas adipiscing. Morbi aliquet eu enim eget condimentum. Nam et bibendum tellus. Curabitur faucibus lectus at enim feugiat placerat. Mauris mattis ligula velit, et volutpat velit facilisis et. In vel massa convallis, commodo nibh non, porttitor orci. Vestibulum vestibulum felis at leo sodales ultricies. Mauris rhoncus vehicula ornare. Donec est ipsum, porta ac neque quis, ornare imperdiet mi. Nulla sit amet vehicula eros. Donec iaculis ante et risus blandit, ut euismod ligula tincidunt. Donec condimentum libero sit amet pharetra condimentum. Praesent eget tortor gravida, rutrum turpis vel, consectetur metus. Sed laoreet erat ante. Mauris eget elit urna.
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam ultricies molestie magna eu condimentum. Suspendisse sagittis dignissim felis a facilisis. In a ipsum arcu. Integer non auctor est, sed consequat turpis. Aliquam erat volutpat. Proin non laoreet sapien. Donec vitae posuere quam. Nullam sollicitudin turpis odio, quis elementum leo pretium vel. Etiam accumsan sodales sapien eu rhoncus. Maecenas sit amet sapien nec diam hendrerit congue eu at enim. Maecenas tempor egestas arcu, a ultricies eros dignissim sed. Praesent tristique orci libero, quis dignissim ante interdum id. Nam eu malesuada ipsum. Suspendisse dictum ipsum ac arcu venenatis fringilla. In id quam ultricies nulla porttitor dictum.
+                    Vivamus rhoncus diam et sagittis vulputate. Maecenas nisl lectus, pharetra ut venenatis sit amet, fringilla sodales elit. Nunc eget vulputate turpis, a mollis est. Nullam rhoncus ac ipsum sed pharetra. Fusce in risus laoreet nulla mattis mollis at a nunc. Maecenas egestas ultrices tellus nec ornare. Duis tempor tellus venenatis urna vulputate, at venenatis odio mollis.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae ligula tincidunt, sodales elit non, tempus purus. Quisque eget purus congue, luctus justo sit amet, sagittis metus. Fusce cursus, tortor in lacinia vehicula, arcu erat viverra metus, id ultricies ante sapien at dolor. Nullam et libero varius, mattis quam nec, rutrum orci. In ac ipsum dapibus est tempor pharetra vitae sit amet magna. Phasellus quis mauris sollicitudin, posuere enim accumsan, varius arcu. Mauris gravida est vel ligula suscipit fringilla. Aliquam erat volutpat. Nunc viverra tincidunt orci ut tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse ut justo eget urna ultricies tempor. Nam sit amet faucibus mauris. Aenean interdum mauris purus, tincidunt porta nisi euismod sed.
+                    Sed sollicitudin vestibulum est eu tincidunt. Aliquam in ante rutrum, pulvinar lorem at, ultrices sem. Morbi tempor volutpat convallis. Aliquam erat volutpat. Praesent dapibus dui nec commodo volutpat. Integer ullamcorper a dolor ac scelerisque. Curabitur vehicula nisi fermentum nulla eleifend, sed ullamcorper lacus porttitor. Sed neque nulla, accumsan at feugiat ut, feugiat a massa. Duis ac dignissim felis. Nam vel faucibus metus.
+                    Vivamus blandit egestas adipiscing. Morbi aliquet eu enim eget condimentum. Nam et bibendum tellus. Curabitur faucibus lectus at enim feugiat placerat. Mauris mattis ligula velit, et volutpat velit facilisis et. In vel massa convallis, commodo nibh non, porttitor orci. Vestibulum vestibulum felis at leo sodales ultricies. Mauris rhoncus vehicula ornare. Donec est ipsum, porta ac neque quis, ornare imperdiet mi. Nulla sit amet vehicula eros. Donec iaculis ante et risus blandit, ut euismod ligula tincidunt. Donec condimentum libero sit amet pharetra condimentum. Praesent eget tortor gravida, rutrum turpis vel, consectetur metus. Sed laoreet erat ante. Mauris eget elit urna.
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam ultricies molestie magna eu condimentum. Suspendisse sagittis dignissim felis a facilisis. In a ipsum arcu. Integer non auctor est, sed consequat turpis. Aliquam erat volutpat. Proin non laoreet sapien. Donec vitae posuere quam. Nullam sollicitudin turpis odio, quis elementum leo pretium vel. Etiam accumsan sodales sapien eu rhoncus. Maecenas sit amet sapien nec diam hendrerit congue eu at enim. Maecenas tempor egestas arcu, a ultricies eros dignissim sed. Praesent tristique orci libero, quis dignissim ante interdum id. Nam eu malesuada ipsum. Suspendisse dictum ipsum ac arcu venenatis fringilla. In id quam ultricies nulla porttitor dictum.
+                    Vivamus rhoncus diam et sagittis vulputate. Maecenas nisl lectus, pharetra ut venenatis sit amet, fringilla sodales elit. Nunc eget vulputate turpis, a mollis est. Nullam rhoncus ac ipsum sed pharetra. Fusce in risus laoreet nulla mattis mollis at a nunc. Maecenas egestas ultrices tellus nec ornare. Duis tempor tellus venenatis urna vulputate, at venenatis odio mollis.
+               </td>
+           </tr>
+        </table>
+
+        <table style="width: 800px; margin: 0 auto;">
+                <tr>
+                    <td rowspan="3"><img src="http://alfadentalteam.es/lib/'.$filae['imagen'].'"></td>
+                    <td>' . $filae['nombre'] . '</td>    
+                </tr>
+                <tr>
+                    <td>' . $filae['direccion'] . '</td>
+                    <td>' . $filae['url'] . '</td>    
+                </tr>
+                <tr>
+                    <td>' . $filae['descripcion'] . '</td>    
+                </tr>
+        </table>
+    </body>
+</html>
+';
+?>
